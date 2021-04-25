1c8fe1d992cdbd731e511215ae3ba0af48c2911b
Villalobos <diegovillalobos397@gmail.com>
2021-04-25 15:21:11 +0200

    QUIZMAAZ-12 Added typescript and tailwindCSS to the next project

--- quiz-maaz-next/.eslintrc.js
+++ quiz-maaz-next/.eslintrc.js
@@ -5,18 +5,25 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb-base',
+    'airbnb-typescript',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     'react',
+    '@typescript-eslint',
   ],
   rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
---
+++ quiz-maaz-next/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      boxShadow: {
+        DEFAULT: '0 1px 3px 0 #ff0094, 0 1px 2px 0 #ff0094',
+      },
+      colors: {
+        textColor: 'black',
+        background: {
+          primary: '#292929',
+          secondary: 'white',
+        },
+        accent: '#ff0094',
+        incorrect: '#ed6a5a',
+        correct: '#c4ffb2',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
